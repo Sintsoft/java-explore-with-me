775c22af3226417575ed98614dc546d490ffe535
Oleg Sintsov <sintsov.o.yu@yandex.ru>
2023-08-31 15:15:01 +0300

    Stats Service with cient builds and runs in docker

---
+++ ewm-statistic/statistic-client/src/main/java/ru/practicum/client/StatisticClient.java
@@ -0,0 +1,46 @@
+package ru.practicum.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import ru.practicum.dto.StatHitRequestDTO;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+public class StatisticClient extends BaseClient {
+
+
+
+    public StatisticClient(@Value("${ewm.statistic-url}") String serverUrl, RestTemplateBuilder builder) {
+        super(
+                builder
+                        .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
+                        .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                        .build()
+        );
+    }
+
+    public void hitUri(StatHitRequestDTO dto) {
+        post("/hit", dto);
+    }
+
+    public ResponseEntity<Object> getStatistic(
+            LocalDateTime start,
+            LocalDateTime end,
+            boolean unique,
+            List<String> uris) {
+        Map<String, Object> parameters = Map.of(
+                "start", start,
+                "end", end,
+                "unique", unique
+        );
+        for (String uri : uris) {
+            parameters.put("uris", uri);
+        }
+        return get("/stats", parameters);
+    }
+}
